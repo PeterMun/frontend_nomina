11795a99815eaa874a6d9d176434569d60014333
Pedro <pedroanibal_jr@hotmail.com>
2023-04-27 14:20:29 -0500

    rutas y mejoramiento de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 import { LoginComponent } from './auth/login/login.component';
 import { Error404Component } from './pages/error404/error404.component';
-import { PagesComponent } from './pages/pages.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ]
 
-  },
-
-  { path: 'login', component: LoginComponent },
-
-  // 
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'   },
   { path: '**', component: Error404Component },
 
 ];
@@ -31,7 +21,9 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot( routes )
+    RouterModule.forRoot( routes ),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [ RouterModule ]
 })
